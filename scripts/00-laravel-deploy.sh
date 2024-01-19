c00daeec3d38d27f9139563ff99e97787244f6f1
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2024-01-20 02:33:09 +0630

    migrate only

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,7 +13,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:fresh --force
+php artisan migrate --force
 
 # echo "Seeding Data..."
 # php artisan db:seed --force
