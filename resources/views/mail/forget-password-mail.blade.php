6c4a728c07ee07b2c8d327175cf5f77add57ab56
Sachio-haise <aungkaungmyatkpg777@gmail.com>
2024-01-29 15:03:05 +0630

    logo url

--- resources/views/mail/forget-password-mail.blade.php
+++ resources/views/mail/forget-password-mail.blade.php
@@ -17,7 +17,7 @@
 
 <body>
     <div style="background-color: #FAFAFA;margin-top: 80px;margin-left:auto;margin-right:auto;width: 450px; height: auto; box-shadow: 0 2px 0 rgba(0, 0, 150, 0.025), 2px 4px 0 rgba(0, 0, 150, 0.015); padding:2rem;font-family: sans-serif;border-radius: 5px">
-        <img src="{{ url('/logo_.png') }}" alt="PKT Education Center" style="width: 80px;margin: 0 auto;display: block;">
+        <img src="https://book-sharing-app-api.onrender.com/logo_.png" alt="PKT Education Center" style="width: 80px;margin: 0 auto;display: block;">
         <div style="text-align:center;margin-bottom: 10px">
             <span style="color:#26DD37;font-size: 18px;font-weight: bold;width: 100%;">
                 Book Sharing App
